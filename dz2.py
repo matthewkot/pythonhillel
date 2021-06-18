6de78cce2013514c2661b54edeeb81202421f3ee
matthew <kotuhua@gmail.com>
2021-06-18 23:33:58 +0300

    dz2(3) Для последней задачи только такое решение придумал

---
+++ dz2.py
@@ -0,0 +1,55 @@
+value = 15
+if value <100:
+    new_value = int(value) / 2
+    print(new_value)
+else:
+  new_value = int(value)[::-1]
+  print(new_value)
+###############################
+
+
+value = 500
+if value <100:
+    new_value = 1
+    print(new_value)
+else:
+  new_value = 0
+  print(new_value)
+################################
+
+
+value = 10
+if value <100:
+    new_value = True
+    print(new_value)
+else:
+  new_value = False
+  print(new_value)
+################################
+
+
+my_str = "slOvo"
+my_str.upper()
+################################
+
+
+my_str = "slOvo"
+my_str.lower()
+################################
+
+
+my_str = "qwer"
+if len(my_str) < 5:
+    print(my_str * 2)
+else :
+    print(my_str)
+################################
+
+
+my_str = "qwer"
+if len(my_str) < 5:
+    back_str = my_str[::-1]
+    print(my_str + back_str)
+else :
+    print(my_str)
+################################
